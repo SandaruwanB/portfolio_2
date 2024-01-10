f504a1a106a0bcf8c8187b9e074f6e1b1140b264
SandaruwanB <sandarusbandara110@gmail.com>
2024-01-11 00:07:16 +0530

    add rrd

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">hello world</h1>
+      <h1 className="text-4xl font-bold underline">hello world</h1>
     </div>
   );
 }
