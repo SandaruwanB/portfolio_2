b02beeb36d789d438ada69baecb2f335e828b79e
SandaruwanB <sandarusbandara110@gmail.com>
2024-09-23 20:50:50 +0530

    [ADD] tailwind css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/home';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
