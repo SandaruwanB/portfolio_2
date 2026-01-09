76caa403d2045e9f5983b434faae43da91dee68e
sandaru-naf <sandaruwanb@newanthoneys.lk>
2026-01-09 23:01:41 +0530

    [DEV] home page styling

---
+++ app/components/projects.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='absolute top-0 left-0'>Projects</div>
+  )
+}
+
+export default Projects
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Projects from "./components/projects"
 
 export default function Home() {
   return (
-    <div>
-      <div>Sandaruwan Bandara</div>
+    <div className="min-h-screen">
+      <Projects />
+      <div className="text-lg text-center">Sandaruwan Bandara</div>
+      <div className="absolute bottom-2 left-0 w-screen">
+        <div className="flex w-full justify-center items-center">
+          <div className="px-5 py-2 bg-white/5 dark:bg-white/10 backdrop-blur-md rounded-lg">
+            <button>Open Projects</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
