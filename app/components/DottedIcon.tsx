f2e2bbe9d6033b52ae7f3725f383e88fdc98c353
SandaruwanB <sandarusbandara110@gmail.com>
2026-01-16 22:35:21 +0530

    [DEV] dotted icons and footer customization

---
+++ app/components/DottedIcon.tsx
@@ -0,0 +1,17 @@
+const DottedIcon = () => {
+    return (
+        <div className="grid grid-cols-3 gap-1">
+            <div className="w-1.5 h-1.5 bg-gray-300 rounded-full group-hover:bg-white transition-colors"></div>
+            <div className="w-1.5 h-1.5 bg-gray-300 rounded-full group-hover:bg-white transition-colors"></div>
+            <div className="w-1.5 h-1.5 bg-gray-300 rounded-full group-hover:bg-white transition-colors"></div>
+            <div className="w-1.5 h-1.5 bg-gray-300 rounded-full group-hover:bg-white transition-colors"></div>
+            <div className="w-1.5 h-1.5 bg-gray-300 rounded-full group-hover:bg-white transition-colors"></div>
+            <div className="w-1.5 h-1.5 bg-gray-300 rounded-full group-hover:bg-white transition-colors"></div>
+            <div className="w-1.5 h-1.5 bg-gray-300 rounded-full group-hover:bg-white transition-colors"></div>
+            <div className="w-1.5 h-1.5 bg-gray-300 rounded-full group-hover:bg-white transition-colors"></div>
+            <div className="w-1.5 h-1.5 bg-gray-300 rounded-full group-hover:bg-white transition-colors"></div>
+        </div>
+    )
+}
+
+export default DottedIcon;
